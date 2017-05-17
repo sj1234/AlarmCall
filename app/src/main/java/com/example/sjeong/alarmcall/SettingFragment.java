9b2d9d313b580e8c1f120f35616c3dd2b87905f9
kjh1992 <wer2wer306@gmail.com>
2017-05-17 17:06:04 +0900

    세팅창

--- app/src/main/java/com/example/sjeong/alarmcall/SettingFragment.java
+++ app/src/main/java/com/example/sjeong/alarmcall/SettingFragment.java
@@ -3,25 +3,30 @@ package com.example.sjeong.alarmcall;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SettingFragment extends Fragment {
+public class SettingFragment extends PreferenceFragment {
     // TODO: Rename parameter arguments, choose names that match
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        addPreferencesFromResource(R.xml.pref_settings);
+        Preference pAppName = (Preference) findPreference("setting_activity_id");
+        Preference pAppVersion = (Preference) findPreference("setting_activity_app_version");
+        CheckBoxPreference cbpAlarmReceive = (CheckBoxPreference) findPreference("setting_activity_alarm_reiceive");
+        Preference pEmail = (Preference) findPreference("sending_email");
+        final SwitchPreference pPushlater =  (SwitchPreference) findPreference("push_later");
+        Preference pBack = (Preference) findPreference("back");
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
-    }
+
 
 
     @Override
