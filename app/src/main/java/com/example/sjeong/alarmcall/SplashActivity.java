2a46d0b99ff38707439dc4932ecc7d97428a6cbd
sj1234 <cu9997@naver.com>
2017-05-24 20:17:21 +0900

    색깔 수정

--- app/src/main/java/com/example/sjeong/alarmcall/SplashActivity.java
+++ app/src/main/java/com/example/sjeong/alarmcall/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         try {
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
