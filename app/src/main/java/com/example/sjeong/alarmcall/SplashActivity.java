42c3e5196d735c20e042c1f3b9eedacc5ea6ca67
sj1234 <cu9997@naver.com>
2017-05-24 19:03:03 +0900

    처음 실행시 로딩화면 SplashActivity 추가

---
+++ app/src/main/java/com/example/sjeong/alarmcall/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.sjeong.alarmcall;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by SJeong on 2017-05-24.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
