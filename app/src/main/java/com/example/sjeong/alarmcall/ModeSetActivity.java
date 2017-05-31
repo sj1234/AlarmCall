1cf8d0d041361595916d8a470d7b32349b81ffe2
YE JOONHYEONG <jhy9142@naver.com>
2017-06-01 01:56:04 +0900

    동그라미

--- app/src/main/java/com/example/sjeong/alarmcall/ModeSetActivity.java
+++ app/src/main/java/com/example/sjeong/alarmcall/ModeSetActivity.java
@@ -2,11 +2,22 @@ package com.example.sjeong.alarmcall;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -29,6 +40,7 @@ import android.widget.Toast;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ModeSetActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -38,6 +50,23 @@ public class ModeSetActivity extends AppCompatActivity implements View.OnClickLi
     private DBManager dbManager;
     private Mode mode;
     private String name;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 2;
+    static final int REQUEST_IMAGE_CROP = 3;
+    ImageView iv_capture;
+    String mCurrentPhotoPath;
+    Uri photoURI, albumURI = null;
+    Boolean album = false;
+
+
+
+
+
+
+
+
+
 
     private TextView startxt,contacttxt,unknowntxt;
     private EditText modename;
@@ -325,9 +354,25 @@ public class ModeSetActivity extends AppCompatActivity implements View.OnClickLi
                     @Override
                     public void onClick(DialogInterface dialog, int which)
                     {
-                        Intent intent = new Intent(Intent.ACTION_PICK);
-                        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-                        startActivityForResult(intent, PICK_FROM_ALBUM);
+                        Intent intent = new Intent();
+                        // Gallery 호출
+                        intent.setType("image/*");
+                        intent.setAction(Intent.ACTION_GET_CONTENT);
+                        // 잘라내기 셋팅
+                        intent.putExtra("crop", "true");
+                        intent.putExtra("aspectX", 0);
+                        intent.putExtra("aspectY", 0);
+                        intent.putExtra("outputX", 200);
+                        intent.putExtra("outputY", 150);
+                        try {
+                            intent.putExtra("return-data", true);
+                            startActivityForResult(Intent.createChooser(intent,
+                                    "사진 어플"), 2);
+                        } catch (ActivityNotFoundException e) {
+                            // Do nothing for now
+                        }
+
+
                     }
                 });
 
@@ -339,97 +384,25 @@ public class ModeSetActivity extends AppCompatActivity implements View.OnClickLi
 
         }
     }
+
+
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode,resultCode,data);
-        if(resultCode != RESULT_OK)
-            return;
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
 
-        switch(requestCode)
-        {
-            case PICK_FROM_ALBUM:
-            {
-                // 이후의 처리가 카메라와 같으므로 일단  break없이 진행합니다.
-                // 실제 코드에서는 좀더 합리적인 방법을 선택하시기 바랍니다.
-                mImageCaptureUri = data.getData();
-                Log.d("SmartWheel",mImageCaptureUri.getPath().toString());
-            }
 
-            case PICK_FROM_CAMERA:
-            {
-                // 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정합니다.
-                // 이후에 이미지 크롭 어플리케이션을 호출하게 됩니다.
-                Intent intent = new Intent("com.android.camera.action.CROP");
-                intent.setDataAndType(mImageCaptureUri, "image/*");
-
-                // CROP할 이미지를 200*200 크기로 저장
-                intent.putExtra("outputX", 200); // CROP한 이미지의 x축 크기
-                intent.putExtra("outputY", 200); // CROP한 이미지의 y축 크기
-                intent.putExtra("aspectX", 1); // CROP 박스의 X축 비율
-                intent.putExtra("aspectY", 1); // CROP 박스의 Y축 비율
-                intent.putExtra("scale", true);
-                intent.putExtra("return-data", true);
-                startActivityForResult(intent, CROP_FROM_iMAGE); // CROP_FROM_CAMERA case문 이동
-                break;
-            }
-            case CROP_FROM_iMAGE:
-            {
-                // 크롭이 된 이후의 이미지를 넘겨 받습니다.
-                // 이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
-                // 임시 파일을 삭제합니다.
-                if(resultCode != RESULT_OK) {
-                    return;
-                }
-                final Bundle extras = data.getExtras();
-                // CROP된 이미지를 저장하기 위한 FILE 경로
-                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()+
-                        "/SmartWheel/"+System.currentTimeMillis()+".jpg";
-                if(extras != null)
-                {
-                    Bitmap photo = extras.getParcelable("data"); // CROP된 BITMAP
-                    imageView.setImageBitmap(photo); // 레이아웃의 이미지칸에 CROP된 BITMAP을 보여줌
-                    storeCropImage(photo, filePath); // CROP된 이미지를 외부저장소, 앨범에 저장한다.
-                    absoultePath = filePath;
-                    break;
-                }
-                // 임시 파일 삭제
-                File f = new File(mImageCaptureUri.getPath());
-                if(f.exists())
-                {
-                    f.delete();
-                }
+        if (requestCode == 2) {
+            Bundle extras2 = data.getExtras();
+            if (extras2 != null) {
+                Bitmap photo = extras2.getParcelable("data");
+                RoundedAvatarDrawable tmpRoundedAvatarDrawable = new RoundedAvatarDrawable(photo);
+                icon.setImageDrawable(tmpRoundedAvatarDrawable);
             }
         }
-
     }
 
-    /*
-    * Bitmap을 저장하는 부분
-    */
-    private void storeCropImage(Bitmap bitmap, String filePath) {
-        // SmartWheel 폴더를 생성하여 이미지를 저장하는 방식이다.
-        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/SmartWheel";
-        File directory_SmartWheel = new File(dirPath);
 
-        if(!directory_SmartWheel.exists()) // SmartWheel 디렉터리에 폴더가 없다면 (새로 이미지를 저장할 경우에 속한다.)
-            directory_SmartWheel.mkdir();
-        File copyFile = new File(filePath);
-        BufferedOutputStream out = null;
-        try {
-
-            copyFile.createNewFile();
-            out = new BufferedOutputStream(new FileOutputStream(copyFile));
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-            // sendBroadcast를 통해 Crop된 사진을 앨범에 보이도록 갱신한다.
-            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
-                    Uri.fromFile(copyFile)));
-
-            out.flush();
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     private int IconImage(int id){
         switch (id){
@@ -461,5 +434,90 @@ public class ModeSetActivity extends AppCompatActivity implements View.OnClickLi
         }
         return null;
     }
+
+}
+
+ class RoundedAvatarDrawable extends Drawable {
+    private final Bitmap mBitmap;
+    private final Paint mPaint;
+    private final RectF mRectF;
+    private final int mBitmapWidth;
+    private final int mBitmapHeight;
+
+    public RoundedAvatarDrawable(Bitmap bitmap) {
+        mBitmap = bitmap;
+        mRectF = new RectF();
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        final BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        mPaint.setShader(shader);
+
+        mBitmapWidth = mBitmap.getWidth();
+        mBitmapHeight = mBitmap.getHeight();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+     //   canvas.drawOval(mRectF, mPaint);
+        canvas.drawCircle(mBitmapWidth/2,mBitmapHeight/2,mBitmapHeight/2,mPaint);
+    }
+
+    @Override
+    protected void onBoundsChange(Rect bounds) {
+        super.onBoundsChange(bounds);
+
+        mRectF.set(bounds);
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        if (mPaint.getAlpha() != alpha) {
+            mPaint.setAlpha(alpha);
+            invalidateSelf();
+        }
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter cf) {
+        mPaint.setColorFilter(cf);
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.TRANSLUCENT;
+    }
+
+    @Override
+    public int getIntrinsicWidth() {
+        return mBitmapWidth;
+    }
+
+    @Override
+    public int getIntrinsicHeight() {
+        return mBitmapHeight;
+    }
+
+    public void setAntiAlias(boolean aa) {
+        mPaint.setAntiAlias(aa);
+        invalidateSelf();
+    }
+
+    @Override
+    public void setFilterBitmap(boolean filter) {
+        mPaint.setFilterBitmap(filter);
+        invalidateSelf();
+    }
+
+    @Override
+    public void setDither(boolean dither) {
+        mPaint.setDither(dither);
+        invalidateSelf();
+    }
+
+    public Bitmap getBitmap() {
+        return mBitmap;
+    }
+
 
 }
